c13abf705cbccfa23b46236387b1ef6579721c0d
Kathleen Devlin <katdev@users.noreply.github.com>
2020-10-21 14:41:06 -0700

    Revert "Added CloudTrail resource (#2)" (#3)
    This reverts commit 0a75814ae3fe7be1f3f16b03abbdd9a8f933d7ab.

--- altimeter/aws/resource/cloudtrail/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Base class for CloudTrail resources."""
-
-from altimeter.aws.resource.resource_spec import AWSResourceSpec
-
-
-class CloudTrailResourceSpec(AWSResourceSpec):
-    """Base class for CloudTrail resources."""
-
-    service_name = "cloudtrail"
